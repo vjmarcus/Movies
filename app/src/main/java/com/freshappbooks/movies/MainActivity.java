d96936f56d905bd8ebb6ec64927a50679232d899
vjmarcus <vjmarcus1@gmail.com>
2019-10-08 13:48:11 +0300

    Add Network JSON Methods

--- app/src/main/java/com/freshappbooks/movies/MainActivity.java
+++ app/src/main/java/com/freshappbooks/movies/MainActivity.java
@@ -3,6 +3,12 @@ package com.freshappbooks.movies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.freshappbooks.movies.utils.NetworkUtils;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.TOP_RATED, 1);
+        if (jsonObject == null) {
+            Toast.makeText(this, "Error with JSON", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/freshappbooks/movies/utils/NetworkUtils.java
+++ app/src/main/java/com/freshappbooks/movies/utils/NetworkUtils.java
@@ -1,9 +1,19 @@
 package com.freshappbooks.movies.utils;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class NetworkUtils {
 
@@ -21,7 +31,7 @@ public class NetworkUtils {
     public static final int POPULARITY = 0;
     public static final int TOP_RATED = 1;
 
-    public static URL buildURL (int sortBy, int page) {
+    private static URL buildURL (int sortBy, int page) {  //Method created URL to API
         URL result = null;
         String methodOfSort;
         if (sortBy == POPULARITY){
@@ -41,5 +51,53 @@ public class NetworkUtils {
             e.printStackTrace();
         }
         return result;
+    }
+
+    public static JSONObject getJSONFromNetwork(int sortBy, int page) {
+        JSONObject result = null;
+        URL url = buildURL(sortBy, page);
+        try {
+
+            result = new JSONLoadTask().execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static class JSONLoadTask extends AsyncTask<URL, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(URL... urls) {
+            JSONObject result = null;
+            if (urls == null && urls.length == 0){
+                return null;
+            }
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) urls[0].openConnection(); // open connection
+                InputStream inputStream = connection.getInputStream();  // get data Stream
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                StringBuilder builder  = new StringBuilder();
+                String line = reader.readLine();
+                while (line != null) {
+                    builder.append(line);
+                    line = reader.readLine();
+                }
+
+                result = new JSONObject(builder.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return result ;
+        }
     }
 }
