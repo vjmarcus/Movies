913a10e6e6cfe53cae39af977a738035ec6c15f6
vjmarcus <vjmarcus1@gmail.com>
2019-10-08 17:27:28 +0300

    Worked as expected

--- app/src/main/java/com/freshappbooks/movies/MainActivity.java
+++ app/src/main/java/com/freshappbooks/movies/MainActivity.java
@@ -6,22 +6,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.freshappbooks.movies.data.Movie;
+import com.freshappbooks.movies.utils.JSONUtils;
 import com.freshappbooks.movies.utils.NetworkUtils;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MyApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.TOP_RATED, 1);
-        if (jsonObject == null) {
-            Toast.makeText(this, "Error with JSON", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
+        Log.v(TAG, jsonObject.toString());
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        StringBuilder builder = new StringBuilder();
+        for (Movie movie : movies) {
+            builder.append(movie.getTitle()).append("\n");
         }
+        Log.v("MyApp", builder.toString());
     }
 }
