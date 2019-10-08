4ff832e0ce2682206daa086287ae4121b1c3e926
vjmarcus <vjmarcus1@gmail.com>
2019-10-08 14:03:41 +0300

    Add Movie class

--- app/src/main/java/com/freshappbooks/movies/data/Movie.java
+++ app/src/main/java/com/freshappbooks/movies/data/Movie.java
@@ -1,4 +1,89 @@
 package com.freshappbooks.movies.data;
 
 public class Movie {
+
+    private int id;
+    private int voteCount;
+    private String title;
+    private String originalTltle;
+    private String posterPath;
+    private String backDropPath;
+    private double voteAverage;
+    private String releaseDate;
+
+
+    public Movie(int id, int voteCount, String title, String originalTltle, String posterPath, String backDropPath, double voteAverage, String releaseDate) {
+        this.id = id;
+        this.voteCount = voteCount;
+        this.title = title;
+        this.originalTltle = originalTltle;
+        this.posterPath = posterPath;
+        this.backDropPath = backDropPath;
+        this.voteAverage = voteAverage;
+        this.releaseDate = releaseDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOriginalTltle() {
+        return originalTltle;
+    }
+
+    public void setOriginalTltle(String originalTltle) {
+        this.originalTltle = originalTltle;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackDropPath() {
+        return backDropPath;
+    }
+
+    public void setBackDropPath(String backDropPath) {
+        this.backDropPath = backDropPath;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
