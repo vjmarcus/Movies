ec673d09442150f50be03c7bd26921d9d761343b
vjmarcus <vjmarcus1@gmail.com>
2019-10-09 16:09:17 +0300

    Add movie item

--- app/src/main/java/com/freshappbooks/movies/MainActivity.java
+++ app/src/main/java/com/freshappbooks/movies/MainActivity.java
@@ -23,13 +23,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
-        Log.v(TAG, jsonObject.toString());
-        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
-        StringBuilder builder = new StringBuilder();
-        for (Movie movie : movies) {
-            builder.append(movie.getTitle()).append("\n");
-        }
-        Log.v("MyApp", builder.toString());
     }
 }
