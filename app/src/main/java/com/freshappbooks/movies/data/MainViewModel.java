a99a7ce1b28a3be695b282ee388cd6855f97be66
vjmarcus <vjmarcus1@gmail.com>
2019-10-10 20:37:44 +0300

    Add MainViewModel

---
+++ app/src/main/java/com/freshappbooks/movies/data/MainViewModel.java
@@ -0,0 +1,87 @@
+package com.freshappbooks.movies.data;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private static MovieDatabase database;
+    private LiveData<List<Movie>> movies;
+
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        database = MovieDatabase.getInstance(getApplication());
+        movies = database.movieDao().getAllMovies();
+    }
+
+    public Movie getMovieById(int id) {
+        try {
+            return new GetMovieTask().execute(id).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void deleteAllMovies() {
+        new DeleteMoviesTask().execute();
+    }
+
+    public void insertMovie(Movie movie) {
+        new InsertMovieTask().execute(movie);
+    }
+
+    public void deleteMovie(Movie movie) {
+        new DeleteTask().execute(movie);
+    }
+
+    private static class DeleteTask extends AsyncTask<Movie, Void, Void> {
+        @Override
+        protected Void doInBackground(Movie... movies) {
+            if (movies != null && movies.length > 0) {
+                database.movieDao().deleteMovie(movies[0]);
+            }
+            return null;
+        }
+    }
+
+    private static class InsertMovieTask extends AsyncTask<Movie, Void, Void> {
+        @Override
+        protected Void doInBackground(Movie... movies) {
+            if (movies != null && movies.length > 0) {
+                database.movieDao().insertMovie(movies[0]);
+            }
+            return null;
+        }
+    }
+
+    private static class DeleteMoviesTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... integers) {
+            if (integers != null && integers.length > 0) {
+                database.movieDao().deleteAllMovies();
+            }
+            return null;
+        }
+    }
+
+    private static class GetMovieTask extends AsyncTask<Integer, Void, Movie> {
+        @Override
+        protected Movie doInBackground(Integer... integers) {
+            if (integers != null && integers.length > 0) {
+                return database.movieDao().getMovieById(integers[0]);
+            }
+            return null;
+        }
+    }
+}
